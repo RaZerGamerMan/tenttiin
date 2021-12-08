7474565689360745be6365ad7bafafd543afb5d9
RaZerGamerMan <ctr-fani@hotmail.com>
2021-12-09 01:42:46 +0200

    neljätoist

---
+++ Kaikkitehtavat/Exercise_Enums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_Enums
+{
+    public enum CarBody
+    {
+        Coupé = 3,
+        Sedan,
+        HatchBack,
+        StationWagon
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)CarBody.Sedan);
+            Console.ReadLine();
+        }
+    }
