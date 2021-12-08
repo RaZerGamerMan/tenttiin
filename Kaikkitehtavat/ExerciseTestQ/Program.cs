dc7c1378f0e5d5ac8c5d54997314f790b784fc6c
RaZerGamerMan <ctr-fani@hotmail.com>
2021-12-09 00:47:40 +0200

    kolmetoist

---
+++ Kaikkitehtavat/ExerciseTestQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Luo luokka Foo
+// Luokalta löytyy funktio public void DoSomething()
+// -tulosta funktiossa jotain konsoliin.
+
+// Luo pääohjelmassa Foo taulukko, jonka koko on 10
+
+// Luo 10 Foo oliota taulukon alkiohin, ja kutsu samalla DoSomething() funktiota.
+namespace ExerciseTestQ
+{
+    class Program
+    {
+        class Foo
+        {
+            public void DoSomething()
+            {
+                Console.WriteLine("Do something!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Foo[] foos = new Foo[10];
+
+            for (int i = 0; i < foos.Length; i++)
+            {
+                foos[i] = new Foo();
+                foos[i].DoSomething();
+            }
+        }
+    }
+}
