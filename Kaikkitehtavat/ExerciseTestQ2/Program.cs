529b0daf5216cc73e7bba6b9f1bc08f63f14c427
RaZerGamerMan <ctr-fani@hotmail.com>
2021-12-09 01:47:06 +0200

    viistoist

---
+++ Kaikkitehtavat/ExerciseTestQ2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ExerciseTestQ2
+{
+    abstract class Character
+    {
+        private int x = 0, y = 0;
+        private int maxbounds = 10;
+
+        protected Character(int x, int y)
+        {
+            SetLocation(x, y);
+        }
+
+        public abstract void Speak();
+
+        public void SetLocation(int x, int y)
+        {
+            if ((x < -maxbounds || x > maxbounds) || (y < -maxbounds || y > maxbounds))
+            {
+                Console.WriteLine("{0} objects new location was out of bounds.", this.GetType());
+                return;
+            }
+
+            this.x = x;
+            this.y = y;
+            Console.WriteLine("{0} moved to location {1}", this.GetType().Name, GetLocation());
+        }
+        public string GetLocation()
+        {
+            return ($"({x},{y})");
+
+        }
+
+    }
+    class Hero : Character
+    {
+        public string name;
+        public int hp;
+        public Hero(string name, int hp, int x, int y) : base(x, y) //koekysymys luultavasti
+        {
+            this.name = name;
+            this.hp = hp;
+
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("I will protect my city!");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kaikkitehtavat/Exercise_Memory/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercise_Memory
+{
+    class GameObject
+    {
+        static GameObject()
+        {
+            Console.WriteLine("New game object created!");
+        }
+    }
+
+    class Scene
+    {
+        private GameObject object1 = null;
+        private GameObject object2 = null;
+        private bool IsRunning = false;
+
+        public Scene()
+        {
+            Init();
+        }
+        public void Init()
+        {
+            object1 = new GameObject();
+            object2 = new GameObject();
+            IsRunning = true;
+        }
+        public void Run()
+        {
+            Console.WriteLine("Game on!");
+            while (IsRunning)
+            {
+                Console.WriteLine("Updating....");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scene gameScene = new Scene();
+            gameScene.Run();
+        }
+    }
+}
+
