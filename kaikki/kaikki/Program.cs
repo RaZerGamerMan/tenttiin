06003fd822c38526b15bcd7a7f50edb9f506e079
RaZerGamerMan <ctr-fani@hotmail.com>
2021-12-08 22:00:55 +0200

    eka

---
+++ kaikki/kaikki/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace kaikki
+{
+    class Program
+    {
+        class Player
+        {
+            public uint id;
+
+
+            public void PrintID()
+            {
+                Console.WriteLine("Player id: " + id);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            player1.PrintID();
+
+        }
+    }
+}
